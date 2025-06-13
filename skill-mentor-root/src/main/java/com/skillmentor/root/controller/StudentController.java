73fc14c78706241360bc8cb0fd2ee2c3b2ed45d9
yasirulakshitha <gamagejanithyasirulakshitha@gmail.com>
2025-06-13 11:40:23 +0530

    update

--- skill-mentor-root/src/main/java/com/skillmentor/root/controller/StudentController.java
+++ skill-mentor-root/src/main/java/com/skillmentor/root/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.skillmentor.root.controller;
 
 
+import com.skillmentor.root.service.impl.StudentServiceImpl;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping
 public class StudentController {
+    static StudentServiceImpl studentService = new StudentServiceImpl();
 }
